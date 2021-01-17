4afa6a6c83a27fecd7de64587b34d507e9faf7a4
MojProfil <gluhak.daniel@users.noreply.github.com>
2021-01-17 21:49:37 +0100

    add user input
    Added user input for X and Y axis

--- eclipse-workspace/ZimskiZadaci/src/zadaci/Zadatak2.java
+++ eclipse-workspace/ZimskiZadaci/src/zadaci/Zadatak2.java
@@ -1,11 +1,25 @@
 package zadaci;
 
+import java.util.Scanner;
+
 public class Zadatak2 {
 
 	public static void main(String[] args) {
 //		Kreirati program koji ispisuje tablicu množenja 15x15 lijepo formatiranu
 		
-		int[][] table = new int[15][15];
+//		update: korisnik unosi velicinu tablice
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter the size of X axis: ");
+		int x = scan.nextInt();
+		
+		System.out.println("Enter the size of Y axis: ");
+		int y = scan.nextInt();
+		
+		scan.close();
+		
+		int[][] table = new int[x][y];
 		
 		for(int i=0; i<table.length; i++) {
 			for(int j=0; j<table.length; j++) {
