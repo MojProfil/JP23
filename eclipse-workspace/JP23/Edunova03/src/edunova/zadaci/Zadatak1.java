ea9d9fab79b07194f1f5f369a56db67bf6c0fdd8
MojProfil <gluhak.daniel@users.noreply.github.com>
2021-01-18 14:41:25 +0100

    ponavljanje

--- eclipse-workspace/JP23/Edunova03/src/edunova/zadaci/Zadatak1.java
+++ eclipse-workspace/JP23/Edunova03/src/edunova/zadaci/Zadatak1.java
@@ -1,12 +1,30 @@
 package edunova.zadaci;
 
 public class Zadatak1 {
-
-	//Program ispisuje sve brojeve od 20 do 200
 	
-	// program ispisuje sve parne brojeve od 1 do 57
-	
-	//program ispisuje sve brojeve od 100 do 1
-	
-	//program ispisuje sve neparne brojeve od 100 do 21
+	public static void main(String[] args) {
+		
+		//Program ispisuje sve brojeve od 20 do 200
+		for(int i = 20; i<=200; i++) {
+			if(i%2==0) {
+				System.out.println(i);	
+			}
+		// program ispisuje sve parne brojeve od 1 do 57
+			for(i = 1; i<=57; i++) {
+				if(i%2==0) {
+					System.out.println(i);	
+				}
+		//program ispisuje sve brojeve od 100 do 1
+				for(i = 100; i>1; i--) {
+						System.out.println(i);	
+					}
+		//program ispisuje sve neparne brojeve od 100 do 21
+				for(i=100; i>=21; i--) {
+					if(i%2 != 0) {
+						System.out.println(i);
+					}
+				}	
+			}
+		}
+	}
 }
--- eclipse-workspace/JP23/Edunova03/src/edunova/zadaci/Zadatak2.java
+++ eclipse-workspace/JP23/Edunova03/src/edunova/zadaci/Zadatak2.java
@@ -1,10 +1,29 @@
 package edunova.zadaci;
 
+import java.util.Scanner;
+
 public class Zadatak2 {
 	
-	// Program unosi dva cijela broja.
-	// Program ispisuje OK ako je zbroj svih parnih brojeva
-	// između ta dva cijela broja
-	// veći od 1000 
-
+	public static void main(String[] args) {
+		// Program unosi dva cijela broja.
+		// Program ispisuje OK ako je zbroj svih parnih brojeva
+		// između ta dva cijela broja
+		// veći od 1000 
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter first number: ");
+		int num1 = scan.nextInt();
+		System.out.println("Enter second number: ");
+		int num2 = scan.nextInt();
+		
+		int count = 0;
+		for(int i = num1; i<num2; i++) {
+			count+=i;
+			if((i%2==0) && (count>1000)) {
+				System.out.println("OK");
+			}
+		}
+		
+		
+	}
 }
