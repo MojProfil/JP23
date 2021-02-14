e0eb6ae1d42ae62257dc84fe3cd3f5d6f980554e
MojProfil <gluhak.daniel@users.noreply.github.com>
2021-02-14 21:32:12 +0100

    Test Intellij workspace

---
+++ idea-workspace/radsbazom/src/edunova10/Start.java
@@ -0,0 +1,12 @@
+package edunova10;
+
+public class Start {
+
+    public Start() {
+        System.out.println("Hello.");
+    }
+
+    public static void main(String[] args) {
+        new Start();
+    }
+}
